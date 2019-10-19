eb1c011f69b8f40244f916b85e68c4154b96d66c
muhammadsarim555 <muhammadsarim555@gmail.com>
2019-10-19 18:48:41 +0500

    Modify - Splash Screen

--- src/Screens/Splash/splash.js
+++ src/Screens/Splash/splash.js
@@ -1,21 +1,23 @@
 import React from "react";
-import { View, StyleSheet, Image } from "react-native";
+import { View, StyleSheet, Image, ImageBackground } from "react-native";
 
 import {
   widthPercentageToDP as wp,
   heightPercentageToDP as hp
 } from "react-native-responsive-screen";
 
+import Banner from "../../../assets/banners/splash.png"
+import Logo from "../../../assets/banners/logo.png"
 export default class splash extends React.Component {
   render() {
     return (
-      <View style={Styles.container}>
+        <ImageBackground source={Banner} style={Styles.container}>
         <Image
-          style={{ width: wp("80%"), height: hp("100%") }}
-          source={require("../../../res/img/deapLearning.png")}
+          style={{ width: wp("60%"), height: hp("100%") }}
+          source={Logo}
           resizeMode={"center"}
         />
-      </View>
+      </ImageBackground>
     );
   }
 }
